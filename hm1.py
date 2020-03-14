f1d186a10e073523520995a9102f39c920233104
devilroop <61988264+devilroop@users.noreply.github.com>
2020-03-14 11:45:02 +0530

    home work(14/3/20

---
+++ hm1.py
@@ -0,0 +1,27 @@
+r=9.2
+h=3
+pi=22/7
+c=(1/3)*pi*(r**2)*h
+area=int(c)
+print(' "area of cone is " ',area)
+r=9.2
+h=3
+pi=22/7
+c=pi*r**2
+d=str(c)
+print("areaof circle is",d)
+print(type(d))
+r=9.2
+h=3
+pi=22/7
+c=2*pi*r
+d=int(c)
+print(' "circumference of circle is" ',d)
+print(type(d))
+n=5
+p=5*12
+r=13/100
+npr=n*p*r
+d=float(c)
+print(' "the value of npr is" ',npr)
+
